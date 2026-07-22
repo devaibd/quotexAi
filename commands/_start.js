bf3cd0a108e160e89b3d1fd0a70476e0dd31f342
devaibd <abirquotexai4.0@gmail.com>
2026-07-22 20:58:35 +0600

    Update _start.js

--- commands/_start.js
+++ commands/_start.js
@@ -1,15 +1,21 @@
 /*CMD
   command: /start
-  help: 
-  need_reply: 
-  auto_retry_time: 
-  answer: Start always run in first. You can see /help
-
-  <<KEYBOARD
-text, link,
-image, 💬 emoji, inline,
-🗲 BJS: Bot Java Script
-  KEYBOARD
-  aliases: back
+  help:
+  need_reply: false
+  auto_retry_time:
+  answer:
+  keyboard:
 CMD*/
 
+Api.sendMessage({
+  text:
+"👋 *Welcome to the VIP QUOTEX AI Signal Bot!*\n\nChoose an option below:\n\n📊 Get Signal\n📚 Risk Rules\n🌐 Open Broker App",
+  parse_mode: "Markdown",
+  reply_markup: {
+    keyboard: [
+      [{ text: "📊 Get Signal" }, { text: "📚 Risk Rules" }],
+      [{ text: "🌐 Open Broker App", web_app: { url: "https://qxbroker.com" } }]
+    ],
+    resize_keyboard: true
+  }
+});
